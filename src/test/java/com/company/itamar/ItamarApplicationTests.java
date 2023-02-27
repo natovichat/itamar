74a2130d58af831480b2d9f4dc3abcb5f6c01eec
aviad <aviad.natovich@digitalturbine.com>
2023-02-27 16:11:02 +0200

    first commit - itamar application - mazal tov

---
+++ src/test/java/com/company/itamar/ItamarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.itamar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItamarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
